b859c310f5b6ad10b0c5ee3d3426d608f6595b85
TilmanGraff <32928249+TilmanGraff@users.noreply.github.com>
2017-11-24 23:44:36 +0000

    Altered to reflect the real data

--- Build/code/create_adj_dist_speed.R
+++ Build/code/create_adj_dist_speed.R
@@ -3,29 +3,54 @@
 
 library("osrm", lib.loc="/Library/Frameworks/R.framework/Versions/3.2/Resources/library")
 
-centroids <- read.csv("Build/temp/country_centroids/Nigeria.csv")
-n <- nrow(centroids)
+# Import clean global centroids file
+centroids <- read.csv("/Users/Tilmanski/Documents/UNI/MPhil/Second Year/Thesis_Git/Build/temp/centroids.csv")
+colnames(centroids)[1] <- "ID"
 
-centroids$rownumber <- c(1:n) # this is to create a later rosetta stone
+# Restrict file sample to Africa
+centroids <- centroids[centroids$region == 2,]
+centroids <- centroids[centroids$country == "Benin",]
+
+
+# Gather country names
+country_table <- as.data.frame(table(centroids$country))
+country_names <- paste(country_table[country_table$Freq != 0,"Var1"])
+
+###############
+
+# For every country, calculate matrices
+for(country in country_names){
+
+case_centroids <- centroids[centroids$country == country,]
+
+n <- nrow(case_centroids)
+
+# this is to create a rosetta stone which translates the (globally unique) cell ID into the rownumber of the square matrices needed for Matlab
+case_centroids$rownumber <- c(1:n)
+
+
+case_centroids$productivity <- case_centroids$lights / case_centroids$pop
+
 
 adj <- matrix(0, nrow = n, ncol = n)
 dist <- matrix(0, nrow = n, ncol = n)
 speed <- matrix(0, nrow = n, ncol = n)
 
-# productivity <- centroids$lights / centroids$population
-productivity <- centroids$ID / centroids$ID # this just creates a file of ones just for show
 
-#for(i in 1:nrow(centroids)){
-  for(i in 1:30){
+for(i in 1:n){
 
-  neighbour_ids <- centroids[(centroids$top-centroids$top[i])^2+(centroids$left-centroids$left[i])^2<=0.25^2+0.25^2,"ID"]
-  df <- centroids[centroids$ID %in% neighbour_ids, c("ID", "X", "Y") ]
+
+# identifies the nine centroids within the respective circle to find itself plus eight nearest neighbors
+  neighbour_ids <- case_centroids[(case_centroids$y-case_centroids$y[i])^2+(case_centroids$x-case_centroids$x[i])^2<=0.5^2+0.5^2,"ID"]
+  df <- case_centroids[case_centroids$ID %in% neighbour_ids, c("ID", "x", "y") ]
   colnames(df) <- c("id", "lon", "lat")
 
-    for(a in neighbour_ids[! neighbour_ids %in% centroids[i, "ID"]]){           # scrapes OSRM for routes to all neighbours excluding itself
-      route <- osrmRoute(src=df[df$id==centroids$ID[i],], dst=df[df$id==a,], sp=TRUE, overview = F)
-      j <- centroids[centroids$ID == a,"rownumber"]
+# scrapes OSRM for routes to all neighbours excluding itself
+    for(a in neighbour_ids[! neighbour_ids %in% case_centroids[i, "ID"]]){
+      route <- osrmRoute(src=df[df$id==case_centroids$ID[i],], dst=df[df$id==a,], sp=TRUE, overview = F)
+      j <- case_centroids[case_centroids$ID == a,"rownumber"]
 
+    # creates matrices for every country
       dist[i, j] <- route[2]
       speed[i, j] <- route[2] / (route[1]/60)
       adj[i, j] <- 1
@@ -34,7 +59,9 @@ productivity <- centroids$ID / centroids$ID # this just creates a file of ones j
 
   }
 
-write.csv(dist, file="Build/temp/dist/Nigeria.csv")
-write.csv(speed, file="Build/temp/speed/Nigeria.csv")
-write.csv(adj, file="Build/temp/adj/Nigeria.csv")
-write.csv(productivity, file="Build/temp/productivity/Nigeria.csv")
+
+write.csv(dist, file=paste("/Users/Tilmanski/Documents/UNI/MPhil/Second Year/Thesis_Git/Build/temp/dist/dist_", country, ".csv", sep=""))
+write.csv(speed, file=paste("/Users/Tilmanski/Documents/UNI/MPhil/Second Year/Thesis_Git/Build/temp/speed/speed_", country, ".csv", sep=""))
+write.csv(adj, file=paste("/Users/Tilmanski/Documents/UNI/MPhil/Second Year/Thesis_Git/Build/temp/adj/adj_", country, ".csv", sep=""))
+write.csv(case_centroids[,c("ID", "rownumber", "productivity", "rugg", "pop")], file=paste("/Users/Tilmanski/Documents/UNI/MPhil/Second Year/Thesis_Git/Build/temp/characteristics/characteristics_", country, ".csv", sep=""))
+}
